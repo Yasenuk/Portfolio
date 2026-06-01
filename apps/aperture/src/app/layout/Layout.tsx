57f05da6cd4b487b639d39c57ca501c10b681120
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-06-01 23:40:21 +0300

    feat(layout): implement layout component with header and footer

---
+++ apps/aperture/src/app/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+import { lazy } from 'react';
+
+const Header = lazy(() => import('./Header'));
+const Footer = lazy(() => import('./Footer'));
+
+
+export default function Layout() {
+	return (
+		<>
+			<Header />
+			<main id="main-content">
+				<Outlet />
+			</main>
+			<Footer />
+		</>
+	);
+}
