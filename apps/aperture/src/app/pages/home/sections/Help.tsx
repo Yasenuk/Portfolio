16522b723441bc00335be9f01b22ccea455b1735
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-06-08 18:36:03 +0300

    feat(Help): implement Help section with content and ButtonMain component

--- apps/aperture/src/app/pages/home/sections/Help.tsx
+++ apps/aperture/src/app/pages/home/sections/Help.tsx
@@ -1,3 +1,13 @@
+import { ButtonMain } from "@portfolio/aperture";
+
 export default function Help() {
-	return (<></>);
+	return (
+		<div className="bg-black">
+			<div className="container pt-20 pb-16 md:pt-[7.5rem] md:pb-[6.25rem] flex flex-col gap-y-4 items-center">
+				<h2 className="text-center text-primary text-[2.5rem] leading-[2.625rem] bottom-4 max-w-[36rem] mx-auto">Need help with photography or videography?</h2>
+				<p className="text-center text-secondary text-2xl">We're here for you!</p>
+				<ButtonMain>Get in touch</ButtonMain>
+			</div>
+		</div>
+	);
 }
