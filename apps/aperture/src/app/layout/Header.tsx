ff9a8a2c5dcb185c8ebdf605d474d33b9b147c54
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-06-10 16:17:03 +0300

    feat(button): implement ButtonPrimary component with variant support and update usage in Header

--- apps/aperture/src/app/layout/Header.tsx
+++ apps/aperture/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import { Backdrop, BurgerMenu, BurgerMenuBody, Trigger } from '@portfolio/shared-ui';
 import { linkClass } from '@portfolio/aperture-shared';
+import { ButtonPrimary } from '@portfolio/aperture';
 
 const navLinks = [
 	{ name: 'Business areas', to: '/' },
@@ -28,12 +29,7 @@ export default function Header() {
 								</Link>
 							))}
 						</nav>
-						<button className={`
-							px-4 py-2 bg-primary text-black rounded-[0.875rem] text-center border-2 border-primary
-							hover:bg-black hover:text-primary transition-colors duration-300
-						`}>
-							Get template
-						</button>
+						<ButtonPrimary variant={'main'}>Get template</ButtonPrimary>
 					</BurgerMenuBody>
 				</BurgerMenu>
 			</div>
--- libs/aperture/src/components/button.tsx
+++ libs/aperture/src/components/button.tsx
@@ -1,26 +1,37 @@
 import * as React from 'react';
-
 import { cn } from '@portfolio/shared-utils';
-import { Button } from "@portfolio/shared-ui";
+import { cva, type VariantProps } from "class-variance-authority";
 
-const buttonClass = cn(`
-	text-sm text-dark rounded-[0.875rem] border border-primary
-	p-2.5 pl-10 relative transition-all duration-300
-	after:absolute after:w-8 after:h-8 after:bg-dark after:rounded-[0.875rem]
-	after:left-1 after:top-1/2 after:-translate-y-1/2
-	hover:text-primary hover:bg-dark hover:px-[1.625rem]
-	hover:after:w-full hover:after:left-0 hover:after:h-full
-	after:transition-all
-`);
+const buttonVariants = cva(
+	`text-sm text-dark bg-primary rounded-[0.875rem] border border-primary p-2.5 relative transition-all duration-300 hover:text-primary`,
+	{
+		variants: {
+			variant: {
+				default: `pl-10 hover:px-[1.625rem] hover:after:w-full hover:after:left-0 hover:after:h-full 
+				after:transition-all after:absolute after:w-8 after:h-8
+				after:bg-dark after:rounded-[0.875rem] after:left-1 after:top-1/2 after:-translate-y-1/2`,
+				main: "hover:bg-dark"
+			}
+		},
+		defaultVariants: {
+			variant: "default"
+		}
+	}
+)
 
-const ButtonPrimary = ({ className, children, ...props }: React.ComponentProps<typeof Button>) => {
-  return (
-    <Button className={cn(buttonClass, className)} {...props}>
-      <span className="z-10">{children}</span>
-    </Button>
-  );
-};
+export interface ButtonProps
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+	VariantProps<typeof buttonVariants> { };
 
+const ButtonPrimary = React.forwardRef<HTMLButtonElement, ButtonProps>(
+	({ className, children, variant = 'default', ...props }, ref) => {
+		return (
+			<button ref={ref} className={cn(buttonVariants({ variant, className }))} {...props}>
+				<span className="relative z-10">{children}</span>
+			</button>
+		)
+	}
+);
 ButtonPrimary.displayName = "ButtonPrimary"
 
 export { ButtonPrimary };
