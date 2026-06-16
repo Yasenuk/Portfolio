81af011b521280ebe807125d50080a725fee00a6
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-06-16 17:51:15 +0300

    feat: add Verticals section to Home page for showcasing expertise areas

--- apps/rhine-media/src/app/pages/home/home.tsx
+++ apps/rhine-media/src/app/pages/home/home.tsx
@@ -1,12 +1,14 @@
 import { ButtonMain } from '@portfolio/rhine-media';
 import Hero from './sections/hero';
 import Stats from './sections/stats';
+import Verticals from './sections/verticals';
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<Stats />
+			<Verticals />
 		</>
 	);
 }
