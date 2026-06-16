8ec5d5c87c1809a223fafe7510340e2efc9fd361
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-06-16 12:07:42 +0300

    feat: update PostCSS and Tailwind configuration for rhine-media application

--- apps/rhine-media/tailwind.config.js
+++ apps/rhine-media/tailwind.config.js
@@ -1,17 +1,19 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nx/next/tailwind');
+const preset = require('@portfolio/tailwind-config/preset');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [preset],
   content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/!(*.stories|*.spec).{ts,tsx,html}',
-    ),
+    './{src,pages,components,app}/**/!(*.stories|*.spec).{ts,tsx,js,jsx,html}',
+    join(__dirname, '../../libs/**/src/**/!(*.stories|*.spec).{ts,tsx,js,jsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 };
