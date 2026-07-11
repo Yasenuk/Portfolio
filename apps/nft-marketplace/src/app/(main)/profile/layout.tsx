6ae84314edc27fed3bca4a0025fe90f99a4106cf
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-07-11 18:35:42 +0300

    feat: add padding to profile layout container for improved responsiveness

--- apps/nft-marketplace/src/app/(main)/profile/layout.tsx
+++ apps/nft-marketplace/src/app/(main)/profile/layout.tsx
@@ -7,7 +7,7 @@ export default async function ProfileLayout({ children }: { children: React.Reac
 	if (!user) redirect('/login');
 
 	return (
-		<div className="container flex flex-col gap-8 py-5 lg:flex-row">
+		<div className="container md:px-0 flex flex-col gap-8 py-5 lg:flex-row">
 			<aside className="lg:w-60 shrink-0 relative">
 				<ProfileNav />
 			</aside>
